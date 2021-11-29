140ce6318d05a0cec8b8725f20938b7830381c9e
sukumar M <sukumar.m@qbrainx.com>
2021-11-29 16:06:19 +0530

    second commit on array

--- src/main/scala/Task/Array.scala
+++ src/main/scala/Task/Array.scala
@@ -17,10 +17,9 @@ var a:Array[Int]=new Array[Int](5)
   a(3)=100
   a(4)=200
 
- var f =(x:Int,y:Int) =>x max y
 
-//println("Min number ===> " + a.reduceLeft(_ min _))
-  println("Max number ===> " + a.reduceLeft(_ max  _))
+println("Min number ===> " + a.reduce(_ min _))
+  println("Max number ===> " + a.reduce(_ max  _))
 
 }
 
