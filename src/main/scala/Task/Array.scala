c9a3121d34f33bf33ad330a0411ebae0b28e9d7f
sukumar M <sukumar.m@qbrainx.com>
2021-11-29 16:02:00 +0530

    array committe

--- src/main/scala/Task/Array.scala
+++ src/main/scala/Task/Array.scala
@@ -3,7 +3,6 @@ package Task
 object Array extends  App {
 
 
-
   /* val m1 = List(5, 12, 3, 13)   //list
  
    val result = m1.max
@@ -11,4 +10,18 @@ object Array extends  App {
    println(result1)
  */
 
+var a:Array[Int]=new Array[Int](5)
+ a(0)=2055
+  a(1)=21
+  a(2)=50
+  a(3)=100
+  a(4)=200
+
+ var f =(x:Int,y:Int) =>x max y
+
+//println("Min number ===> " + a.reduceLeft(_ min _))
+  println("Max number ===> " + a.reduceLeft(_ max  _))
+
 }
+
+
